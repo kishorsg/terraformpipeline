d0d3d33dd05bd16dadce0dcedd5318f429992d74
Kishor Ghanekar <48198031+kishorsg@users.noreply.github.com>
2020-06-12 18:43:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-  access_key = "${var.aws_access_key}"
-  secret_key = "${var.aws_secret_key}"
+  access_key = "${var.aws-access-key}"
+  secret_key = "${var.aws-secret-key}"
   region     = "${var.aws_region}"
 }
 
